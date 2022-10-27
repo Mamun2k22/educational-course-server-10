5bf410e5e56d8a070d4755c4adca00bb31d078c0
Mamun2k22 <mamun180037@gmail.com>
2022-10-27 18:21:55 +0600

    1st server git commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const courses = require('./Data/courses.json')
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Course Details Running');
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const singleCourses = courses.find(n => n._id === id)
+    res.send(singleCourses)
+})
+app.listen(port, () => {
+    console.log('Course Details Server running on', port);
+})
